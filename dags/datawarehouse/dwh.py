b246639be40f08be70a301fe8e1a71c11fd9b5ff
Filipe <30056747+GitHubFilipe@users.noreply.github.com>
2025-11-25 21:03:32 +0100

    Completed dwh.py (core schema)

--- dags/datawarehouse/dwh.py
+++ dags/datawarehouse/dwh.py
@@ -27,6 +27,7 @@ def staging_table():
         table_ids = get_video_ids(cur, schema)
 
         for row in YT_data:
+            # eventually better outside of loop 
             if len(table_ids) == 0:
                 insert_rows(cur, conn, schema, row)
 
@@ -39,6 +40,57 @@ def staging_table():
         ids_in_json = {row['video_id'] for row in YT_data}
 
         ids_to_delete = set(table_ids) - ids_in_json
+
+        if ids_to_delete:
+            delete_rows(cur, conn, schema, ids_to_delete)   
+
+        logger.info(f"{schema} table update completed")
+
+    except Exception as e:
+        logger.error(f"An error occured during the update of {schema} table: {e}")
+        raise e
+    
+    finally:
+        if conn and cur:
+            close_conn_cursor(conn, cur)
+
+
+@task
+def core_table():
+
+    schema = "core"
+    conn,cur = None, None
+
+    try:
+
+        conn,cur = get_conn_cursor()
+
+        create_schema(schema)
+        create_table(schema)
+
+        table_ids = get_video_ids(cur, schema)
+
+        current_video_ids = set()
+
+        cur.execute(f"SELECT * FROM staging.{table};")
+        rows = cur.fetchall()
+
+        for row in rows:
+            current_video_ids.add(row['Video_ID'])
+
+            if len(table_ids) == 0:
+                transformed_row = transform_data(row)
+                insert_rows(cur, conn, schema, transformed_row)
+
+            else:
+                transformed_row = transform_data(row)
+
+                if transformed_row["Video_ID"] in table_ids:
+                    update_rows(cur, conn, schema, transformed_row)
+                else:
+                    insert_rows(cur, conn, schema, transformed_row)
+
+        ids_to_delete = set(table_ids) - current_video_ids
 
         if ids_to_delete:
             delete_rows(cur, conn, schema, ids_to_delete)   
