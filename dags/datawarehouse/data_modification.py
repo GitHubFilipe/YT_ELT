2feb32c634fafd1a00720632bc6a96a92f6eeed2
Filipe <30056747+GitHubFilipe@users.noreply.github.com>
2025-11-26 22:42:46 +0100

    completed dag and fixed bugs

--- dags/datawarehouse/data_modification.py
+++ dags/datawarehouse/data_modification.py
@@ -11,17 +11,19 @@ def insert_rows(cur, conn, schema, row):
             video_id = 'video_id'
 
             cur.execute(
-                f"""INSERT INTO {schema}.{table} ("Video_ID", "Video_Title", "Upload_Date", "Duration", "Video_Views", "Likes_Count", "Comments_Count")
+                f"""
+                INSERT INTO {schema}.{table}("Video_ID", "Video_Title", "Upload_Date", "Duration", "Video_Views", "Likes_Count", "Comments_Count")
                 VALUES (%(video_id)s, %(title)s, %(publishedAt)s, %(duration)s, %(viewCount)s, %(likeCount)s, %(commentCount)s);
-               """, row
+                """,
+                row,
             )
         else:
             video_id = "Video_ID"
 
             cur.execute(
                 f"""INSERT INTO {schema}.{table} ("Video_ID", "Video_Title", "Upload_Date", "Duration", "Video_Type", "Video_Views", "Likes_Count", "Comments_Count")
-                VALUES (%(Video_ID)s, %(Video_Title)s, %(Upload_Date)s, %(Duration)s, %(video_Type)s, %(Video_Views)s, %(Likes_Count)s, %(Comments_Count)s);
-               """, row
+                VALUES (%(Video_ID)s, %(Video_Title)s, %(Upload_Date)s, %(Duration)s, %(Video_Type)s, %(Video_Views)s, %(Likes_Count)s, %(Comments_Count)s);
+               """, row,
             )
 
         conn.commit()
--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -4,7 +4,7 @@ from psycopg2.extras import RealDictCursor
 table = "yt_api"
 
 def get_conn_cursor():
-    hook = PostgresHook(postgres_conn_id='postfres_db_yt_elt', database='elt_db')
+    hook = PostgresHook(postgres_conn_id='postgres_db_yt_elt', database='elt_db')
     conn = hook.get_conn()
     cur = conn.cursor(cursor_factory=RealDictCursor)
     return conn, cur
@@ -16,7 +16,7 @@ def close_conn_cursor(conn, cur):
 def create_schema(schema):
     conn, cur = get_conn_cursor()
     schema_sql = f"CREATE SCHEMA IF NOT EXISTS {schema};"
-    cur.execute(schema)
+    cur.execute(schema_sql)
     conn.commit()
     close_conn_cursor(conn, cur)
 
